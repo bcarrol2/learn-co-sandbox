4193022ae7cc1e8ab4aaacf8066490c6a32fabca
bcarrol2 <blairacarroll@gmail.com>
2018-09-30 19:54:50 +0000

    automatically backed up by learn

---
+++ conference_badges.rb
@@ -0,0 +1,40 @@
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(room)
+
+end
+
+rooms = [1, 2, 3, 4, 5, 6, 7]
+
+def assign_room(name, number)
+  puts "Hello, #{name}! You'll be assigned to room #{number}!"
+end
+
+[names, rooms].each_with_index do |value, index|
+end
+
+def printer(print)
+
+end
+
+printer(0)
+
+badge_maker(names[0])
+badge_maker(names[1])
+badge_maker(names[2])
+badge_maker(names[3])
+badge_maker(names[4])
+badge_maker(names[5])
+badge_maker(names[6])
+
+assign_room(names[0], rooms[0])
+assign_room(names[1], rooms[1])
+assign_room(names[2], rooms[2])
+assign_room(names[3], rooms[3])
+assign_room(names[4], rooms[4])
+assign_room(names[5], rooms[5])
+assign_room(names[6], rooms[6])
